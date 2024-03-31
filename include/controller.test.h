35e755b18a11cb9edff48436ba9b4c98e22cfa34
Hamza BAYA <bayahamza1010@gmail.com>
2024-03-31 15:05:41 +0200

    Update controller.test.h

--- include/controller.test.h
+++ include/controller.test.h
@@ -1,9 +1,4 @@
-/**
- * \file controller.test.h
- * \brief Fichier de test de controller.h
- * \author nicolas.bernardi@uha.fr
- * \version 0.1
-*/
+
 #ifndef CONTROLLER_TEST_H
 #define CONTROLLER_TEST_H
 
