7bfd6df6dd7223a72761eaa8650ef5e74066d789
Hamza BAYA <bayahamza1010@gmail.com>
2024-03-31 15:06:10 +0200

    Update message_display.h

--- include/message_display.h
+++ include/message_display.h
@@ -1,9 +1,4 @@
-/**
- * \file message_display.h
- * \brief Fichier permettant d'afficher des messages à l'utilisateur.
- * \author simon.antropius@uha.fr
- * \version 0.1
- *
+/*
  * Ce fichier contient des fonctions affichant des messages à l'utilisateur sur 
  * la fin de jeu, les erreurs et le réseau.
  */
